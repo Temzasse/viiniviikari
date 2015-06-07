4a94e876e1a36116090addd4d361c07231a2d577
Joel Lappalainen <Joel@Jopu.Elisa>
2015-06-07 20:13:48 +0300

    jee

--- app/scripts/controllers/posts.controller.js
+++ app/scripts/controllers/posts.controller.js
@@ -12,16 +12,68 @@ angular
 	.controller('PostsCtrl', function(){
 		/* jshint validthis: true */
 		var vm = this;
-		vm.testi = 'moi';
+		vm.allposts = [
+			{
+				title : 'viini 1',
+				imgUrl : 'https://c1.staticflickr.com/1/458/18379325189_72aa346583_c.jpg',
+				details : 
+					[
+						{
+							label : 'Rypäle',
+							content : 'Shiraz Cabernet'
+						},
+						{
+							label : 'Vuosikerta',
+							content : '1997'
+						},
+						{
+							label : 'Maa',
+							content : 'Australia'
+						}
 
+					],
+				description : 'aivan huikee viini',
+				rating : 9.7
+			},
+			{
+				title : 'viini 2',
+				imgUrl : 'https://c1.staticflickr.com/1/458/18379325189_72aa346583_c.jpg',
+				details : 
+					[
+						{
+							label : 'Rypäle',
+							content : 'Shiraz Cabernet'
+						}
+					],				
+				description : 'ei sovi kalan kanssa',
+				rating : 6.7			
+			},
+			{
+				title : 'viini 3',
+				imgUrl : 'https://c1.staticflickr.com/1/458/18379325189_72aa346583_c.jpg',
+				details : 
+					[
+						{
+							label : 'Rypäle',
+							content : 'Shiraz Cabernet'
+						}
+					],				
+				description : 'ainoa laatuaan',
+				rating : 9.9			
+			}
+		];
+
+		vm.getPosts = getPosts;
 		activate();
-		return vm;
 		/////////////////////
 
 
 
 		function activate(){
 			console.log('hae kaikki postit');
+		}
+		function getPosts(){
+
 		}
 	});
 
